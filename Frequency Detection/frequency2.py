7f56970aa973a5f0ee9cbfb9056ac592821962ad
Akshay Damle <akshaydamle95@rediffmail.com>
2015-12-30 15:46:19 +0530

    Code now outputs frequencies and powers at 2nd 3rd and 4th harmonics

--- Frequency Detection/frequency2.py
+++ Frequency Detection/frequency2.py
@@ -3,17 +3,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fftpack
 
-input_data = wav.read('low_pitched_voice.wav')
+input_data = wav.read('.wav')
 
 # Number of samplepoints
 N = len(input_data[1])#600
 # sample spacing
 T = 1.0 / input_data[0]
 
-ps = np.abs(np.fft.fft(input_data[1]))**2
-freqs = np.fft.fftfreq(N, T)
-idx = np.argsort(freqs)
+window_length = 4410
 
-plt.plot(freqs[idx], ps[idx])
+freqlist = []
 
-plt.show()
+for i in range(0, N - window_length, window_length):
+	data = input_data[1][i:i+window_length]
+
+	ps = np.abs(np.fft.fft(data*np.hanning(window_length)))**2
+	freqs = np.fft.fftfreq(window_length, T)
+	idx = np.argsort(freqs)
+
+	freqs = freqs[idx]
+	ps = ps[idx]
+
+	freqs = freqs[len(freqs)/2:]
+	ps = ps[len(ps)/2:]
+
+	# plt.plot(freqs, ps)
+	# plt.show()
+
+	index = np.where(ps==max(ps))[0][0]
+
+	try:
+		princi = freqs[np.where(ps == max(ps[:index-10]))[0][0]]
+		freqlist.append(princi)
+	except:
+		pass
+
+	f = freqs[index]
+
+	
+
+print freqlist
+print np.mean(freqlist)
+print len(freqlist)
